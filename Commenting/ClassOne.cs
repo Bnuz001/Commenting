210703ec291dd6e5adb44e724791acdc77b52cf5
Christian Fröberg <Bnuz001@users.noreply.github.com>
2022-12-09 12:56:05 +0100

    Updated
    Things

---
+++ Commenting/ClassOne.cs
@@ -0,0 +1,53 @@
+﻿///<summary>
+/// Namn: Christian Fröberg
+/// Klass: SY21
+/// Info:
+/// I denna fil finns mina klasser för genomgången om kommentering.
+///</summary>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommentProgram
+{
+    /// <summary>
+    /// En klass för att exemplifiera kommentering
+    /// </summary>
+    class ClassOne
+    {
+        /// <value>
+        /// ParaOne är en Propertie
+        /// </value>
+        private int ParaOne;
+        /// <value>
+        /// 
+        /// </value>
+        private int ParaTwo;
+
+        /// <summary>
+        /// Konstruktor för klassen ClassOne
+        /// </summary>
+        /// <param name="paraOne">Förklaring av paraOne</param>
+        /// <param name="paraTwo">Förklaring av paraTwo</param>
+        public ClassOne(int paraOne, int paraTwo)
+        {
+            ParaOne = paraOne;
+            ParaTwo = paraTwo;
+        }
+        /// <summary>
+        /// En förklaring av metoden
+        /// </summary>
+        /// <param name="paraThree">Förklaring av paraThree</param>
+        /// <returns>Summan av paraOne, paraTwo och paraThree</returns>
+        public int methodOne(int paraThree)
+        {
+            return ParaOne + ParaTwo + paraThree;
+        }
+    
+    }
+}
+
+
---
+++ Commenting/Program.cs
@@ -0,0 +1,5 @@
+﻿using CommentProgram;
+
+ClassOne firstObj = new ClassOne(1, 2);
+
+Console.WriteLine(firstObj.methodOne(3));
